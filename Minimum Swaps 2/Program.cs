13336f4fffe6fd9041a08641f7317e00d63dd94c
JESUS SANJUAN MENDEZ <j.sanjuanmendez@ugto.mx>
2020-05-30 12:22:32 -0500

    Agregar archivos de proyecto.

---
+++ Minimum Swaps 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace Minimum_Swaps_2
+{
+    class Program
+    {
+        static int minimumSwaps(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                while (arr[i] != i + 1)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[temp - 1];
+                    arr[temp - 1] = temp;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            // Ordena un vector de manera acendente sin importar el tamaño, en la menor cantidad de movimientos 
+            int[] arr = new int[4] { 4, 3, 1, 2 };
+            int res = minimumSwaps(arr);
+            Console.Read();
+        }
+    }
+}
